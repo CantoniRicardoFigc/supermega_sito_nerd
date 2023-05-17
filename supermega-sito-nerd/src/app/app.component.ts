36a8a83ee80ec57927383ec60bb7edc3cb0e1d8b
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2023-05-17 13:02:28 +0000

    versione search quasi finita

--- supermega-sito-nerd/src/app/app.component.ts
+++ supermega-sito-nerd/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Root } from './tigre/tigre.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'supermega-sito-nerd';
-  constructor() {
-
-  }
+  cartaSearch !: String;
+  obsCard !: Observable<Root>;
+  ris !: Root;
+  constructor(private http : HttpClient) {}
+  
   ricercaApi(cartaCercata : HTMLInputElement) {
-    
+    this.cartaSearch = cartaCercata.value;
+    this.obsCard = this.http.get<Root>('https://api.scryfall.com/cards/named?fuzzy=' + this.cartaSearch)
+    this.obsCard.subscribe((data : Root)=>(this.ris = (data)))
   }
 }
